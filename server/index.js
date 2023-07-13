d6e78cadccc9e948877890e9943b48919bd768fa
Vansh Chauhan <96383954+Vansh351@users.noreply.github.com>
2023-07-13 03:17:42 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://learn-wave-frontend-9q2iurjhc-vansh351.vercel.app",
 		credentials:true,
 	})
 )
